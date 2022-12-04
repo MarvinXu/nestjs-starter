dcee410195424c54f3af9c92cb4a575e6f250733
MarvinXu <272077995@qq.com>
2022-12-04 17:09:06 +0800

    chore: hmr with swc
    fix: hmr

---
+++ webpack-hmr.config.js
@@ -0,0 +1,30 @@
+const nodeExternals = require('webpack-node-externals');
+const { RunScriptWebpackPlugin } = require('run-script-webpack-plugin');
+
+module.exports = function (options, webpack) {
+  options.module.rules[0].use.splice(0, 1, {
+    loader: 'swc-loader',
+  });
+  return {
+    ...options,
+    // devtool: 'source-map',
+    entry: ['webpack/hot/poll?100', options.entry],
+    externals: [
+      nodeExternals({
+        allowlist: ['webpack/hot/poll?100'],
+      }),
+    ],
+    plugins: [
+      ...options.plugins,
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.WatchIgnorePlugin({
+        paths: [/\.js$/, /\.d\.ts$/],
+      }),
+      new RunScriptWebpackPlugin({
+        name: options.output.filename,
+        autoRestart: false,
+        // nodeArgs: ['--enable-source-maps'],
+      }),
+    ],
+  };
+};
